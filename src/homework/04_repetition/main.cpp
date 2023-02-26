30408986170e4882a775546c5881bf1bb5f5cb79
Otter2022 <74218306+Otter2022@users.noreply.github.com>
2023-02-26 00:43:41 -0600

    Homework 4

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,7 +1,9 @@
 //write include statements
-
+#include "dna.h"
+#include <iostream>
 //write using statements
-
+using std::cout;
+using std::cin;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +14,32 @@ user enters a y or Y.
 */
 int main() 
 {
+	int selection;
+	int num;
+	int num1;
+	int num2;
+	do {
+		cout<<"***Main Menu***"<<"\n";
+		cout<<"1-Factorial"<<"\n";
+		cout<<"2-Greatest Common Divisor"<<"\n";
+		cout<<"3-exit"<<"\n";
+		cout<<"Enter number for function you would like to select:";
+		cin>>selection;
+
+		if (selection == 1) {
+			cout<<"Enter number for factorial:";
+			cin>>num;
+			cout<<"\n";
+			cout<<factorial(num)<<"\n";
+		} else if (selection == 2) {
+			cout<<"Enter first number:";
+			cin>>num1;
+			cout<<"\n";
+			cout<<"Enter second Number:";
+			cin>>num2;
+			cout<<"\n";
+			cout<<gcd(num1,num2)<<"\n";
+		}
+	} while(selection != 3);
 	return 0;
 }
